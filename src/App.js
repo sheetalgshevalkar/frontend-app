d5879504658d7de11dd65998e7cd8b48f105690d
sheetal <sheetalshevalkar265@gmail.com>
2023-03-13 18:23:25 +0530

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get(process.env.REACT_APP_API_URL + '/api/data').then((response) => {
+      setData(response.data);
+    });
+    
+  
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{data.title}</h1>
+      <p>{data.description}</p>
     </div>
   );
 }
